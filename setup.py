f5e75e393a4805b9b23620b281294976c6bd3eee
simonzack <simonzack@email.com>
2014-03-24 04:16:23 +1100

    converted into a python package

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup, find_packages
+
+setup(
+	name='hfilesize',
+	description='Human Readable File Sizes',
+
+	author='simonzack',
+	author_email='simonzack@gmail.com',
+
+	packages=find_packages(),
+)
