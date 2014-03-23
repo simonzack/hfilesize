c8962a635d2769973327eeae6c8a8358772d052b
simonzack <simonzack@email.com>
2014-03-24 04:48:42 +1100

    updated setup.py for pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,36 @@
+#!/usr/bin/env python3
 
+import re
 from setuptools import setup, find_packages
+
+try:
+	import pypandoc
+
+	with open('README.md') as file:
+		long_description = pypandoc.convert(file.read(), 'rst', format='md')
+		#pandoc bug workaround
+		long_description = re.sub(r'(:alt:\s*(.+)\s*\r?\n)\s*\r?\n\s*\2', r'\1', long_description)
+
+except ImportError:
+	long_description = ''
 
 setup(
 	name='hfilesize',
+	version='0.1.0',
+	license='GPLv3',
 	description='Human Readable File Sizes',
+	long_description=long_description,
+	url='https://github.com/simonzack/hfilesize',
 
 	author='simonzack',
 	author_email='simonzack@gmail.com',
 
 	packages=find_packages(),
+
+	classifiers = [
+		'Development Status :: 4 - Beta',
+		'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+		'Programming Language :: Python :: 3',
+		'Topic :: Software Development :: Libraries :: Python Modules',
+	],
 )
